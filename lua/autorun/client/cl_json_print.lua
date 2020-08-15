21b1ed7968877b5872549109bd8ac66c9f52a0ff
Maxim Ammosov <34907875+Bilwin@users.noreply.github.com>
2020-08-15 16:57:56 +0900

    New streaming

--- lua/autorun/client/cl_json_print.lua
+++ lua/autorun/client/cl_json_print.lua
@@ -2,3 +2,8 @@ net.Receive("json_print", function()
 	local bytes = util.JSONToTable(net.ReadString())
 	chat.AddText(bytes[1] or Color(50,255,255), bytes[2], Color(255,255,255), bytes[3])
 end)
+
+net.Receive('ix_JsonPrint', function()
+	local bytes = util.JSONToTable(net.ReadString())
+	chat.AddText(bytes[2] .. ' | ' .. bytes[1])
+end)
